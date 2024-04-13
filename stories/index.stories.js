ed1cb382e53872c2921dcc3e7bc62fdce0d7bcb2
Reed Jones <4346136+undefined@users.noreply.github.com>
2024-04-13 04:48:11 +0000

    added left/right sidebars

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,24 @@
 import "https://unpkg.com/alpinejs";
 import "../src/index.js";
 
+
+import "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+     ;
+
+
+import "../src/style.css";
+
+
+
 export default {
   parameters: {
     
   },
 };
 
-export const story1 = () => `<my-counter></my-counter>`;
+export const story1 = () => `
+
+
+
+
+<my-counter></my-counter>`;
